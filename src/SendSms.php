15e54bd8289478959c6e751317cd2abd7251b6cd
Praveen Kumar <praveen.42000@gmail.com>
2022-11-13 02:22:48 +0530

    single sms sending functionality implemented

---
+++ src/SendSms.php
@@ -0,0 +1,33 @@
+<?php
+
+namespace Techunico\BulkSmsSender;
+
+require '../vendor/autoload.php';
+
+use Curl\Curl;
+
+class SendSms {
+    
+    const SMS_URL = 'http://bulksms.techunico.com/vb/apikey.php';
+    private $apiKey;
+    private $senderId;
+
+    public function __construct($apiKey, $senderId) {
+        $this->apiKey = $apiKey;
+        $this->senderId = $senderId;
+    }
+
+    public function sendSms($message, $contact,$templatId) {
+        $curl = new Curl();
+        $curl->get(self::SMS_URL, [
+            'apikey' => $this->apiKey,
+            'senderid' => $this->senderId,
+            'templateid' => $templatId,
+            'number' => $contact,
+            'message' => $message,
+        ]);
+        
+        return $curl;
+    }
+
+}
---
+++ vendor/composer/installed.php
@@ -0,0 +1,32 @@
+<?php return array(
+    'root' => array(
+        'pretty_version' => '1.0.0+no-version-set',
+        'version' => '1.0.0.0',
+        'type' => 'library',
+        'install_path' => __DIR__ . '/../../',
+        'aliases' => array(),
+        'reference' => NULL,
+        'name' => 'techunico/bulk_sms_sender',
+        'dev' => true,
+    ),
+    'versions' => array(
+        'curl/curl' => array(
+            'pretty_version' => '2.4.0',
+            'version' => '2.4.0.0',
+            'type' => 'library',
+            'install_path' => __DIR__ . '/../curl/curl',
+            'aliases' => array(),
+            'reference' => 'ba385e40c5907850c555db24505c127fe29f1e44',
+            'dev_requirement' => false,
+        ),
+        'techunico/bulk_sms_sender' => array(
+            'pretty_version' => '1.0.0+no-version-set',
+            'version' => '1.0.0.0',
+            'type' => 'library',
+            'install_path' => __DIR__ . '/../../',
+            'aliases' => array(),
+            'reference' => NULL,
+            'dev_requirement' => false,
+        ),
+    ),
+);
